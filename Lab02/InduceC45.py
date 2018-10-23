0e87e00e8885ef975ed487201d7915e022d3b3c4
Lucas Robertson <lucasrobertson0@gmail.com>
2018-10-23 00:23:10 -0700

    Finished validation

--- Lab02/InduceC45.py
+++ Lab02/InduceC45.py
@@ -13,31 +13,42 @@ from C45Util import *
 
 THRESHOLD = 0.10001
 
-def main():
-    if not (len(sys.argv) in [3, 4]):
-        print("python InduceC45.py <domainFile.xml> " +
-        "<trainingSetFile.csv> [<restrictions.csv>]")
-        return
 
-    attr = parse_attr(sys.argv[1])
-    data = parse_data(sys.argv[2])
-    categ = parse_categ(sys.argv[1])
+def induce_c45(domain_filename, csv_filename, restrictions_filename=None, input_data=None):
+    attr = parse_attr(domain_filename)
+    data = None
+    if input_data:
+        data = input_data
+    else:
+        data = parse_data(csv_filename)
+    categ = parse_categ(domain_filename)
 
     # Puts returnes data in a standardized form
     data = sanitize_data(attr, data, categ)
 
     # Impose restrictions if provided
-    if len(sys.argv) == 4:
-        rest = parse_rest(sys.argv[3], sys.argv[1])
+    if restrictions_filename:
+        rest = parse_rest(restrictions_filename, csv_filename)
         rest_attr(rest, data)
 
     tree = C45Node()
     tree.C45_algorithm(attr, data, categ, THRESHOLD)
     xmltree = tree.to_xml_tree("C45DecisionTree")
 
-    print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+    #print("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
     xmltree.write("C45DecisionTree.xml")
+    return "C45DecisionTree.xml"
 
+def main():
+    if not (len(sys.argv) in [3, 4]):
+        print("python InduceC45.py <domainFile.xml> " +
+        "<trainingSetFile.csv> [<restrictions.csv>]")
+        return
+
+    if len(sys.argv) == 4:
+        induce_c45(sys.argv[1], sys.argv[2], sys.argv[3])
+    else:
+        induce_c45(sys.argv[1], sys.argv[2])
 
 if __name__=="__main__":
     main()
