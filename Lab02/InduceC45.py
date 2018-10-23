ae1d12788ad6f2fd74f7909fc8f9a25f27966880
Ryan Gelston <rgelston@calpoly.edu>
2018-10-22 19:26:09 -0700

    Changed InduceC45.py to work with changes in C45Node

--- Lab02/InduceC45.py
+++ Lab02/InduceC45.py
@@ -11,13 +11,14 @@ import sys
 from C45Node import C45Node
 from C45Util import *
 
-THRESHOLD = 0.05
+THRESHOLD = 0.10001
 
 def main():
     if not (len(sys.argv) in [3, 4]):
         print ("python InduceC45.py <domainFile.xml> " + 
         "<trainingSetFile.csv> [<restrictions.csv>]")
         return
+
     attr = parse_attr(sys.argv[1])
     data = parse_data(sys.argv[2])
     categ = parse_categ(sys.argv[1])
@@ -32,10 +33,11 @@ def main():
 
     tree = C45Node()
     tree.C45_algorithm(attr, data, categ, THRESHOLD)
-    xmltree = tree.to_xml_tree("C45DecisionTree", attr, categ)
+    xmltree = tree.to_xml_tree("C45DecisionTree")
 
     print "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     xmltree.write(sys.stdout)
+
 
 if __name__=="__main__":
     main()
